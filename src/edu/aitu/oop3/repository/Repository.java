c4cf3ee18b8645e32c93d4ca77c2a48e23487c63
Baglan Adilbay <250863@astanait.edu.kz>
2026-02-05 16:16:30 +0300

    Baglan Adilbay – add in memory repository

--- src/edu/aitu/oop3/repository/Repository.java
+++ src/edu/aitu/oop3/repository/Repository.java
@@ -4,12 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface Repository<T> {
-
     void add(T item);
-
     List<T> getAll();
-
     Optional<T> findById(String id);
-
     boolean removeById(String id);
 }
